962e6619db76108b6efab49d17cd7d2109e8e188
vars1ty <gh@resistant.ch>
2024-09-06 09:30:55 +0200

    compiler: Reduce build size

--- build_x64.sh
+++ build_x64.sh
@@ -1,4 +1,4 @@
 # /bin/bash
 # cargo +nightly build -Z build-std=std --target x86_64-pc-windows-gnu --release <-- No XWin
 #RUSTFLAGS="-Z threads=1 -C target-feature=-crt-static -C link-arg=-Wl" cargo +nightly xwin build --release --target x86_64-pc-windows-msvc $@
-RUSTFLAGS="-Z threads=8" cargo +nightly xwin build --release --target x86_64-pc-windows-msvc $@
+RUSTFLAGS="-Clink-args=-fuse-ld=lld -Clink-args=-Wl,--icf=all -Z threads=8 -Z location-detail=none" cargo +nightly xwin build --release --target x86_64-pc-windows-msvc $@
