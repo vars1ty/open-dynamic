0b3891219cac2bbea05c2b9a6d03a981620633d5
vars1ty <gh@resistant.ch>
2025-01-14 06:52:05 +0100

    release: 7.1.0

--- src/utils/api.rs
+++ src/utils/api.rs
@@ -38,7 +38,7 @@ impl API {
         .use_string(|data| {
             // Update this once the version changes, since using env!() doesn't inline the version for
             // it to be encrypted.
-            if zencstr!("7.0.0-release") != zencstr!(data) {
+            if zencstr!("7.1.0-release") != zencstr!(data) {
                 crash!("[ERROR] Invalid version, switch to ", data)
             }
 
